59459fd914250554ba9cdbc7b0e976aaa9fe89fb
Marco Cristofolini <mf.cristofolini@gmail.com>
2018-12-20 02:37:53 +0000

    yeah

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ set -e
 sudo apt-get install -y git-ftp
 
 
-# deploy fazendo upload via ftp, utilizando o lftp para isso
+# deploy fazendo upload via ftp, utilizando o git-ftp para isso
 git ftp init --user $FTP_USER --passwd $FTP_PASS ftp://$FTP_HOST/public_html/lab-travisci.devopszone
