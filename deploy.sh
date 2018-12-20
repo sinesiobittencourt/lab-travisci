ed5923e68f60f68c966e2d8ee5628a59dfec27f1
Marco Cristofolini <mf.cristofolini@gmail.com>
2018-12-20 21:36:23 +0000

    finishing

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 set -e
 
 # deploy fazendo upload via ftp, utilizando o git-ftp para isso
-lftp -c "set net:timeout 5; set ftp:ssl-allow no; open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -Rv ~/public .; quit;"
+lftp -c "set net:timeout 5; set ftp:ssl-allow no; open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -Rv public/ .; quit;"
