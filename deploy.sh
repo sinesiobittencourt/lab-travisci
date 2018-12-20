a9584e8fa3154a9fc4617d976ed90825630a4397
Marco Cristofolini <mf.cristofolini@gmail.com>
2018-12-20 03:06:21 +0000

    teste

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ sudo apt-get install -y git-ftp
 
 # deploy fazendo upload via ftp, utilizando o git-ftp para isso
 #git stash
-git reset --hard && git ftp --user $FTP_USER --passwd $FTP_PASS ftp://$FTP_HOST/public_html/lab-travisci.devopszone
+git reset --hard && git ftp init --user $FTP_USER --passwd $FTP_PASS ftp://$FTP_HOST/public_html/lab-travisci.devopszone && git ftp push --syncroot public --user $FTP_USER --passwd $FTP_PASS ftp://$FTP_HOST/public_html/lab-travisci.devopszone
