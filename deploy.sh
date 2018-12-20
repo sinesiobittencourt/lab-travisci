3ce9761e7c6e99391c35c8cc369d8eb5bbf324b4
root <root@Marco-PC.localdomain>
2018-12-20 00:24:40 +0000

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 # deploy.sh
 set -e
 
-sudo apt-get install lftp
+sudo apt-get install -y lftp
 
 
-# deployment via ftp upload. Using FTPS for that
+# deploy fazendo upload via ftp, utilizando o lftp para isso
 lftp -c "set ftps:initial-prot ''; set ftp:ssl-allow no; open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -eRv public .; quit;"
