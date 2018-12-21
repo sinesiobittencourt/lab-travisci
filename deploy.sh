7340a78a62fbafbf73e3dcb71b32aab65d8b0b29
Marco Cristofolini <mf.cristofolini@gmail.com>
2018-12-21 01:00:13 +0000

    teste

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,3 @@ set -e
 
 # deploy fazendo upload via ftp, utilizando o git-ftp para isso
 lftp -c "set net:timeout 5; set ftp:ssl-allow no; open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -Rv public .; quit;"
-echo "Deploy realizado com sucesso"
